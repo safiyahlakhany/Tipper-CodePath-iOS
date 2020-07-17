eaa71109fb7e32405d76112ad817ec518ff4ec2a
Safiyah Lakhany <salakhany@gmail.com>
2020-07-17 13:38:08 -0700

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Safiyah Lakhany on 7/17/20.
+//  Copyright © 2020 Safiyah Lakhany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
